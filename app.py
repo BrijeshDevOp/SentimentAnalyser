249af0d3214b78153f79dda2e417b9b8a0365c77
BrijeshDevOp <brijesh9663dev@gmail.com>
2025-06-21 14:16:54 +0530

    Configured for render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from model import predict_sentiment
 
@@ -13,4 +14,5 @@ def index():
     return render_template("index.html", sentiment=sentiment)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
